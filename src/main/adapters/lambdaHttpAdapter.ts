039d13a2e7b6ca6e3ebbdfc79a8dada289f79ce0
Lucas Franco <lcs.franco556@gmail.com>
2025-09-16 17:16:25 -0300

    feat: add lambda http adapter with comprehensive error handling and di

---
+++ src/main/adapters/lambdaHttpAdapter.ts
@@ -0,0 +1,77 @@
+import {
+  APIGatewayProxyEventV2,
+  APIGatewayProxyEventV2WithJWTAuthorizer,
+  APIGatewayProxyResultV2,
+} from "aws-lambda";
+import { ZodError } from "zod";
+
+import { Controller } from "@application/contracts/Controller";
+import { ApplicationError } from "@application/errors/app/ApplicationError";
+import { ErrorCode } from "@application/errors/ErrorCode";
+import { HttpError } from "@application/errors/http/HttpError";
+import { Registry } from "@kernel/di/Registry";
+import { lambdaBodyParser } from "@main/utils/lambdaBodyParser";
+import { lambdaErrorResponse } from "@main/utils/lambdaErrorResponse";
+import { Constructor } from "@shared/types/Contructor";
+
+type Event = APIGatewayProxyEventV2 | APIGatewayProxyEventV2WithJWTAuthorizer;
+
+export function lambdaHttpAdapter(
+  controllerImpl: Constructor<Controller<any, unknown>>
+) {
+  return async (event: Event): Promise<APIGatewayProxyResultV2> => {
+    try {
+      const controller = Registry.getInstance().resolve(controllerImpl);
+
+      const body = lambdaBodyParser(event.body);
+      const params = event.pathParameters ?? {};
+      const queryParams = event.queryStringParameters ?? {};
+      // This "internalId" needs to be the same name off the inject custom attribute on Cognito user pool
+      const accountId =
+        "authorizer" in event.requestContext
+          ? (event.requestContext.authorizer.jwt.claims.internalId as string)
+          : null;
+
+      const response = await controller.handle({
+        body,
+        params,
+        queryParams,
+        accountId,
+      });
+
+      return {
+        statusCode: response.statusCode,
+        body: response.body ? JSON.stringify(response.body) : undefined,
+      };
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return lambdaErrorResponse({
+          statusCode: 400,
+          code: ErrorCode.VALIDATION,
+          message: error.issues.map((issue) => ({
+            field: issue.path.join("."),
+            message: issue.message,
+          })),
+        });
+      }
+
+      if (error instanceof HttpError) {
+        return lambdaErrorResponse(error);
+      }
+
+      if (error instanceof ApplicationError) {
+        return lambdaErrorResponse({
+          statusCode: error.statusCode ?? 400,
+          code: error.code,
+          message: error.message,
+        });
+      }
+
+      return lambdaErrorResponse({
+        statusCode: 500,
+        code: ErrorCode.INTERNAL_SERVER_ERROR,
+        message: "Internal server error",
+      });
+    }
+  };
+}
