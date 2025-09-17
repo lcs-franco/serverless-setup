fe58011d1c2ec5354ebc7ec03319c205228bf228
Lucas Franco <lcs.franco556@gmail.com>
2025-09-17 18:30:21 -0300

    remove: delete obsolete hello function file

--- src/hello.ts
+++
@@ -1,8 +0,0 @@
-export async function handler() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
-    }),
-  };
-}
