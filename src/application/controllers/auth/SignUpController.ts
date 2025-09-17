ac681ba316294e5865f746e15347f2e808afd1c9
Lucas Franco <lcs.franco556@gmail.com>
2025-09-17 15:51:30 -0300

    feat: add signup controller with validation schema

---
+++ src/application/controllers/auth/SignUpController.ts
@@ -0,0 +1,44 @@
+import { Controller } from "@application/contracts/Controller";
+import { SignUpUseCase } from "@application/useCases/auth/SignUpUseCase";
+import { Injectable } from "@kernel/decorators/Injectable";
+import { Schema } from "@kernel/decorators/Schema";
+import { signUpSchema, SignUpSchema } from "./schemas/signUpSchema";
+
+@Injectable()
+@Schema(signUpSchema)
+export class SignUpController extends Controller<
+  "public",
+  SignUpController.Response
+> {
+  constructor(private readonly signUpUseCase: SignUpUseCase) {
+    super();
+  }
+
+  protected override async handle({
+    body,
+  }: Controller.Request<"public", SignUpSchema>): Promise<
+    Controller.Response<SignUpController.Response>
+  > {
+    const { email, password } = body;
+
+    const { accessToken, refreshToken } = await this.signUpUseCase.execute({
+      email,
+      password,
+    });
+
+    return {
+      statusCode: 201,
+      body: {
+        accessToken,
+        refreshToken,
+      },
+    };
+  }
+}
+
+export namespace SignUpController {
+  export type Response = {
+    accessToken: string;
+    refreshToken: string;
+  };
+}
