e110932abce68133b38c4718bccf5eb4a6930d21
Lucas Franco <lcs.franco556@gmail.com>
2025-09-17 15:17:49 -0300

    feat: add account entity with ksuid generation and external id support

---
+++ src/application/entities/Account.ts
@@ -0,0 +1,24 @@
+import KSUID from "ksuid";
+
+export class Account {
+  readonly id: string;
+  readonly email: string;
+  externalId: string | undefined;
+  readonly createdAt?: Date;
+
+  constructor(attributes: Account.Attributes) {
+    this.id = attributes.id ?? KSUID.randomSync().string;
+    this.email = attributes.email;
+    this.externalId = attributes.externalId;
+    this.createdAt = attributes.createdAt ?? new Date();
+  }
+}
+
+export namespace Account {
+  export type Attributes = {
+    id?: string;
+    email: string;
+    externalId?: string;
+    createdAt?: Date;
+  };
+}
