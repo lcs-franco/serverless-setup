83a8d8b748a0bf45628fddf5c13f9ae68d75f250
Lucas Franco <lcs.franco556@gmail.com>
2025-09-17 15:18:11 -0300

    feat: add signup use case with account creation and auth gateway integration

---
+++ src/application/useCases/auth/SignUpUseCase.ts
@@ -0,0 +1,51 @@
+import { Account } from "@application/entities/Account";
+import { AuthGateway } from "@infra/gateways/AuthGateway";
+import { Injectable } from "@kernel/decorators/Injectable";
+
+@Injectable()
+export class SignUpUseCase {
+  constructor(private readonly authGateway: AuthGateway) {}
+
+  async execute({
+    email,
+    password,
+  }: SignUpUseCase.Input): Promise<SignUpUseCase.Output> {
+    // const emailAlreadyInUse = await this.accountRepo.findByEmail(email);
+
+    // if (emailAlreadyInUse) {
+    //   throw new Error("Email already in use");
+    // }
+
+    const account = new Account({ email });
+
+    const { externalId } = await this.authGateway.signUp({
+      email,
+      password,
+      internalId: account.id,
+    });
+
+    account.externalId = externalId;
+
+    // const { accessToken, refreshToken } = await this.authGateway.signIn({
+    //   email,
+    //   password,
+    // });
+
+    return {
+      accessToken: "",
+      refreshToken: "",
+    };
+  }
+}
+
+export namespace SignUpUseCase {
+  export type Input = {
+    email: string;
+    password: string;
+  };
+
+  export type Output = {
+    accessToken: string;
+    refreshToken: string;
+  };
+}
